da020fe2563b6eeee8c72ed198b28029b40a6d53
Ihor Krysenko <ihorkr@wix.com>
2025-04-24 21:55:21 +0300

    Add skeleton

---
+++ orders-service/src/test/java/edu/duan/app/ordersservice/OrdersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.duan.app.ordersservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
